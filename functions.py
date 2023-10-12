11ece3affa99528b8036b38283e94095dfa6356e
Skerdouzgit config --global user.name Skerdouzgit config --global user.name Skerdouz <lucasbrahinski60@gmail.com>
2023-10-12 19:07:00 +0200

    added lowercase,uppercase and clean_url func made dump_results and dump_urls less of a mess

--- functions.py
+++ functions.py
@@ -13,20 +13,39 @@ def normalize_url(url):
     return urlunparse(parts)
 
 
+def lowercase_first(word: str) -> str:
+    if not word:
+        return ""
+
+    return word[0].lower() + word[1:]
+
+
+def uppercase_first(word: str) -> str:
+    if not word:
+        return ""
+
+    return word[0].upper() + word[1:]
+
+
 def load_keywords(filename):
     with open(filename, 'r') as json_file:
         data = json.load(json_file)
         return data.get('keywords', [])
 
 
+def clean_url(url):
+    # only for the results/urls dump
+    for pattern in ["http://", "https://", "www."]:
+        url = url.replace(pattern, "")
+    return url.replace("/", "_").replace(".", "_")
+
+
 def dump_results(results, output_directory, input_url):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    filename = os.path.join(output_directory,
-                            input_url.replace("http://", "").replace("https://", "").replace("www.", "").replace(
-                                "/", "_").
-                            replace(".", "_") + ".json")
+    filename = os.path.join(output_directory, clean_url(input_url) + ".json")
+
     with open(filename, 'w', encoding='utf-8') as json_file:
         json.dump(results, json_file, indent=4, ensure_ascii=False)
 
@@ -35,9 +54,7 @@ def dump_urls(wanted_urls, output_directory, input_url):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
 
-    filename = os.path.join(output_directory,
-                            input_url.replace("http://", "").replace("https://", "").replace("www.", "").replace(
-                                "/", "_").
-                            replace(".", "_") + "_wanted.json")
+    filename = os.path.join(output_directory, clean_url(input_url) + "_wanted.json")
+
     with open(filename, 'w') as json_file:
         json.dump(wanted_urls, json_file, indent=4)
